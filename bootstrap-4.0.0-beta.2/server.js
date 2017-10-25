869e5953e48d94ae3566c5b13ca633be5d39d50a
MICHAEL HEDIN <MICHAEL HEDIN>
2017-10-24 23:20:42 -0600

    updates

---
+++ bootstrap-4.0.0-beta.2/server.js
@@ -0,0 +1,270 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request for the news section of ycombinator
+  request("https://news.ycombinator.com/", function(error, response, html) {
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
+    // For each element with a "title" class
+    $(".title").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
+
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.scrapedData.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log(inserted);
+          }
+        });
+      }
+    });
+  });
+
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
+
+
+// Listen on port 3000
+app.listen(3001, function() {
+  console.log("App running on port 3000!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/* Note Taker (18.2.6)
+ * backend
+ * ==================== */
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var app = express();
+
+// Set the app up with morgan, body-parser, and a static folder
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static("public"));
+
+// Database configuration
+var databaseUrl = "week18day2";
+var collections = ["notes"];
+
+// Hook mongojs config to db variable
+var db = mongojs(databaseUrl, collections);
+
+// Log any mongojs errors to console
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+
+// Routes
+// ======
+
+// Simple index route
+app.get("/", function(req, res) {
+  res.send(index.html);
+});
+
+// Handle form submission, save submission to mongo
+app.post("/submit", function(req, res) {
+  console.log(req.body);
+  // Insert the note into the notes collection
+  db.notes.insert(req.body, function(error, saved) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the note back to the browser
+    // This will fire off the success function of the ajax request
+    else {
+      res.send(saved);
+    }
+  });
+});
+
+// Retrieve results from mongo
+app.get("/all", function(req, res) {
+  // Find all notes in the notes collection
+  db.notes.find({}, function(error, found) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send json of the notes back to user
+    // This will fire off the success function of the ajax request
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Select just one note by an id
+app.get("/find/:id", function(req, res) {
+
+  // When searching by an id, the id needs to be passed in
+  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
+
+  // Find just one result in the notes collection
+  db.notes.findOne({
+    // Using the id in the url
+    "_id": mongojs.ObjectId(req.params.id)
+  }, function(error, found) {
+    // log any errors
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    // Otherwise, send the note to the browser
+    // This will fire off the success function of the ajax request
+    else {
+      console.log(found);
+      res.send(found);
+    }
+  });
+});
+
+
+// Update just one note by an id
+app.post("/update/:id", function(req, res) {
+  
+  // When searching by an id, the id needs to be passed in
+  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
+
+  // Update the note that matches the object id
+  db.notes.update({
+    "_id": mongojs.ObjectId(req.params.id)
+  }, {
+    // Set the title, note and modified parameters
+    // sent in the req's body.
+    $set: {
+      "title": req.body.title,
+      "note": req.body.note,
+      "modified": Date.now()
+    }
+  }, function(error, edited) {
+    // Log any errors from mongojs
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    // Otherwise, send the mongojs response to the browser
+    // This will fire off the success function of the ajax request
+    else {
+      console.log(edited);
+      res.send(edited);
+    }
+  });
+});
+
+
+// Delete One from the DB
+app.get("/delete/:id", function(req, res) {
+  // Remove a note using the objectID
+  db.notes.remove({
+    "_id": mongojs.ObjectID(req.params.id)
+  }, function(error, removed) {
+    // Log any errors from mongojs
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    // Otherwise, send the mongojs response to the browser
+    // This will fire off the success function of the ajax request
+    else {
+      console.log(removed);
+      res.send(removed);
+    }
+  });
+});
+
+
+// Clear the DB
+app.get("/clearall", function(req, res) {
+  // Remove every note from the notes collection
+  db.notes.remove({}, function(error, response) {
+    // Log any errors to the console
+    if (error) {
+      console.log(error);
+      res.send(error);
+    }
+    // Otherwise, send the mongojs response to the browser
+    // This will fire off the success function of the ajax request
+    else {
+      console.log(response);
+      res.send(response);
+    }
+  });
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
---
+++ public/app.js
@@ -0,0 +1,21 @@
+/* Note Taker (18.2.6)
+ * front-end
+ * ==================== */
+
+// Loads results onto the page
+function getResults() {
+  // Empty any results currently on the page
+  
+  $.getJSON("/all", function(data) {
+    // For each note...
+    for (var i = 0; i < data.length; i++) {
+      // ...populate #results with a p-tag that includes the note's title and object id
+      $("#results").prepend("<p class='dataentry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
+        data[i]._id + ">" + data[i].title + "</span><span class=deleter>X</span></p>");
+    }
+  });
+}
+
+// Runs the getResults function as soon as the script is executed
+getResults();
+
