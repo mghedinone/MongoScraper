dcf12a273411202733ec0184b5aa569fc1236918
MICHAEL HEDIN <MICHAEL HEDIN>
2017-10-25 18:24:10 -0600

    another port update

--- server.js
+++ server.js
@@ -84,9 +84,10 @@ app.get("/scrape", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(5000, function() {
-  console.log("App running on port 5000!");
-});
+//app.listen(5000, function() {
+  app.listen(process.env.PORT || 5000){
+  console.log("App running on port 3000!");
+};
 
 
 
